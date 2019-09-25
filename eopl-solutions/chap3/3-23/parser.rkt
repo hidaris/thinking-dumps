70b10951a352d9503d491a23c208dc61dc8591eb
hidaris <izuo@outlook.com>
2019-09-25 14:56:23 +0800

    rewrite 3.23

---
+++ eopl-solutions/chap3/3-23/parser.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+(require "ast.rkt")
+(provide (all-defined-out))
+
+;; easy version
+;; (: string->sexp (-> String Any))
+(define (string->sexp s)
+  (read (open-input-string s)))
+
+;; (: def? (Any -> Boolean))
+(define (def? x)
+  (match x
+    [(list 'proc _ _) #t] ;; ((proc _ _) x) or (f x) or ((f x ...) y)
+    [(? symbol? x)    #t]
+    [(list (? symbol? x) _ ...)    #t]
+    [(list (? list? x) _ ...) #t]
+    [_                #f]))
+
+;; (: list-of-symbol? (Any -> Boolean))
+(define (list-of-symbol? x)
+  (andmap symbol? x))
+
+;; (: parse-sexp (-> Any Expression))
+(define (parse-sexp sexp)
+  (match sexp
+    [(? real? x) (Const x)]
+    [(? symbol? x) (Var x)]
+    [`(zero? ,n)
+     (IsZero (parse-sexp n))]
+    [`(if ,test ,then ,else)
+     (If (parse-sexp test)
+         (parse-sexp then)
+         (parse-sexp else))]
+    [`(let ,(? symbol? var) ,val in ,body)
+     (Let var
+          (parse-sexp val)
+          (parse-sexp body))]
+    [`(letproc ,(? symbol? name)
+               ,(? list-of-symbol? var) = ,pbody
+               in ,ebody)
+     (LetProc name
+              var
+              (parse-sexp pbody)
+              (parse-sexp ebody))]
+    [`(- ,n)
+     (Minus (parse-sexp n))]
+    [`(proc ,(? list-of-symbol? var) ,body)
+     (Proc var (parse-sexp body))]
+    [`(,f ,arg ...)
+     (cond
+       [(def? f)
+        (App (parse-sexp f)
+             (map parse-sexp (cdr sexp)))]
+       [else
+        (error 'parse
+               "bad app form ~s" sexp)])
+     ]))
+
+;; (: parse (-> String Program))
+(define (parse str)
+  (let ([sexp (string->sexp str)])
+    (AProgram (parse-sexp sexp))))
