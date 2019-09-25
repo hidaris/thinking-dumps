b5e1f0909f2fcac6d352cab11587a8ba3b0d7ce7
hidaris <izuo@outlook.com>
2019-09-25 14:57:13 +0800

    remove old 3.23

---
+++ compiler/dupe/ast.rkt
@@ -0,0 +1,8 @@
+#lang racket
+;; type Expr =
+;; | Integer
+;; | Boolean
+;; | `(add1 ,Expr)
+;; | `(sub1 ,Expr)
+;; | `(zero? ,Expr)
+;; | `(if ,Expr ,Expr ,Expr)
---
+++ compiler/dupe/interp.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(provide (all-defined-out))
+
+;; type Value =
+;; | Integer
+;; | Boolean
+
+;; Expr -> Value
+(define (interp e)
+  (match e
+    [(? integer? i) i]
+    [(? boolean? b) b]
+    [`(add1 ,e0)
+     (add1 (interp e0))]
+    [`(sub1 ,e0)
+     (sub1 (interp e0))]
+    [`(zero? ,e0)
+     (zero? (interp e0))]
+    [`(if ,e0 ,e1 ,e2)
+     (if (interp e0)
+         (interp e1)
+         (interp e2))]))
